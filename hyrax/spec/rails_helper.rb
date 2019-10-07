3f5fbab00c3449910d8d767a3563e818fb7f1580
Kosuke Tanabe <TANABE.Kosuke@nims.go.jp>
2019-10-07 16:59:40 +0900

    override users_controller to restrict access to users list

--- hyrax/spec/rails_helper.rb
+++ hyrax/spec/rails_helper.rb
@@ -11,6 +11,7 @@ require 'support/input_support'
 # require 'capybara/rspec'  # might need to turn this on in future
 
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'devise'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -59,4 +60,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
